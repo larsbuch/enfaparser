0da149c10206f8d7af0db9dc57a4da555d3fb182
Lars Shakya buch-Jepsen <lbj@applied-intelligence.dk>
2017-05-01 15:42:56 +0200

    Regex Builder finished

--- ENFA_Parser/ENFA_PlaceHolder.cs
+++ ENFA_Parser/ENFA_PlaceHolder.cs
@@ -9,24 +9,11 @@ namespace ENFA_Parser
     public class ENFA_PlaceHolder: ENFA_Base
     {
         private string _groupName;
-        private int? _groupNumber;
 
         //A state* can be a placeholder for a recorded group
         public ENFA_PlaceHolder(string groupName) : base(StateType.NotApplicable)
         {
             _groupName = groupName;
-        }
-        public ENFA_PlaceHolder(int groupNumber) : base(StateType.NotApplicable)
-        {
-            _groupNumber = groupNumber;
-        }
-
-        public int? GroupNumber
-        {
-            get
-            {
-                return _groupNumber;
-            }
         }
 
         public string GroupName
